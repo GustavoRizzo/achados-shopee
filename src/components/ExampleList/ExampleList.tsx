c33d951aa4d9525717b86dddef76dc63379a5bcb
GustavoRizzo <grizzo.albu@gmail.com>
2023-02-08 22:28:56 -0300

    Add aniation to ExampleList component

--- src/components/ExampleList/ExampleList.tsx
+++ src/components/ExampleList/ExampleList.tsx
@@ -1,6 +1,7 @@
 import style from './style.module.scss';
 import mockupData from '../../api/mockupData.json';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function ExampleList() {
 
@@ -15,23 +16,29 @@ export default function ExampleList() {
             <h2>Busque uma pessoa</h2>
             <input placeholder='Digite um nome' onChange={event => setQuery(event.target.value)} />
 
-            <ul className={style.list}>
+            <motion.div className={style.list}>
+                <AnimatePresence>
                 {listPeople
                 .filter((item) =>{
-                    if (query.toLowerCase() === '')
-                        return item    // se busca vazia, retorna todos os resultados
-                    else {
-                        const fields_that_should_be_searched = `${item.first_name} ${item.gender}`;
-                        return fields_that_should_be_searched.toLowerCase().includes(query);
-                    }
-                })                
-                .map((item, index) => (
-                    <li className={style.card} key={index}>
-                        <h3>{item.first_name}</h3>
-                        <span>{item.gender}</span>
-                    </li>
-                ))}
-            </ul>
+                        if (query.toLowerCase() === '')
+                            return item    // se busca vazia, retorna todos os resultados
+                        else {
+                            const fields_that_should_be_searched = `${item.first_name} ${item.gender}`; // junta os campos que devem ser pesquisados
+                            return fields_that_should_be_searched.toLowerCase().includes(query);
+                        }
+                    })                
+                    .map((item, index) => (
+                        <motion.div className={style.card} key={index}
+                            animate={{ opacity: 1 }}
+                            initial={{ opacity: 0 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <h3>{item.first_name}</h3>
+                            <span>{item.gender}</span>
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
+            </motion.div>
 
         </>
     );
